1b179664af2602d379c997f37746f12c7219a6ad
Aaron <arolio301911@gmail.com>
2023-05-03 10:29:04 -0400

    Cambios(A)

--- Models/Products.cs
+++ Models/Products.cs
@@ -5,4 +5,11 @@ public class Products
     [Key]
     public int ProductId {get; set;}
     public string Name {get; set;} = string.Empty;
+
+    [Range(1, 500000, ErrorMessage = "El rango del valor es de {1} y {2}")]
+    public float Price {get; set;}
+    public DateTime EntryDate { get; set; } = DateTime.Today;
+
+
+
 }
