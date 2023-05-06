a89bf5e54974855f8216c59deb9abf9cfff24d7e
Levid <jassontejada@gmail.com>
2023-05-05 21:10:42 -0400

    Adding new field

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,10 +5,9 @@ public class Product
     [Key]
     public int ProductId {get; set;}
     public string Name {get; set;} = string.Empty;
-
     [Range(1, 10000, ErrorMessage = "El rango del valor es de {1} y {2}")]
     public float Price {get; set;}
     public string Brand{get;set;} = string.Empty;
     public DateTime EntryDate { get; set; } = DateTime.Today;
-
+    public int Existence { get; set; }
 }
