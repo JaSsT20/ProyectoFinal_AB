d6ebcfbd4f294e0f410b2b0fbc23e0e441e7028c
Aaron <arolio301911@gmail.com>
2023-05-06 19:50:16 -0400

    Adding Sales Model (fixed)

---
+++ Models/Sale.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+
+public class Sales
+{
+[Key]
+public int SaleId { get; set; }
+public int ClientId { get; set; }
+public int Quantity { get; set; }
+public DateTime SaleDate { get; set; }
+public float Total { get; set; }
+
+} 
